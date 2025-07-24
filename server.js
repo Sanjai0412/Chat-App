f509ee413afa43899f5ad3cca95f5262c7fae493
Sanjai0412 <sanjaikanth44@gmail.com>
2025-07-24 21:53:29 +0530

    Update: Replaced routs by express.static

--- server.js
+++ server.js
@@ -18,13 +18,7 @@ let log = {};
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/signup", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "signUp.html"));
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
   socket.on("login-validation", async (data) => {
